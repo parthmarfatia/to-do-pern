6b96a8828546e261108ef4c0d61c12894f6cf90a
Parth Marfatia <parthmarfatia153@gmail.com>
2023-05-26 02:42:41 +0530

    connecting database to server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const pool = require('./db')
 
 //middleware
 app.use(cors())
 app.use(express.json())
+
+// Routes //
+
+//create a todo
+
+//get all todo
+
+//get a todo
+
+//update a todo
+
+//delete a todo
+
 
 app.listen(5000,()=>{
     console.log('app has started')
